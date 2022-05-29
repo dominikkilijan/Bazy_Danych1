457110571be07cfe92f0f8e2aa5ea760ede1ecaf
dominikkilijan <dominikkilijan@gmail.com>
2022-05-29 23:42:37 +0200

    Transaction

--- Main.sql
+++ Main.sql
@@ -1,2 +1,12 @@
 USE bazy_danych1;
 
+source createDB.sql;
+source Types.sql;
+source Address.sql;
+source Employee.sql;
+source Client.sql;
+source Account.sql;
+source CommonAccount.sql;
+source Contract.sql;
+source Card.sql;
+source Transaction.sql;
